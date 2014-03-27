60d890d2b07a7bcc6759c53f56ed5c0e9c25c001
igorcoding <igorcoding@gmail.com>
2014-03-27 13:02:37 +0400

    Refactored tornado-server to work on gen.Task

--- tornado-server/server.py
+++ tornado-server/server.py
@@ -1,3 +1,4 @@
+from functools import wraps
 import json
 import logging
 import threading
@@ -5,64 +6,47 @@ import time
 from api import api_executor
 from api.util.response_helpers import *
 
-from tornado import web
+from tornado import web, gen
 import tornado.httpserver
 import tornado.ioloop
 from tornado.options import define, options
 
 
-class ThreadMixin(web.RequestHandler):
-    def start_worker(self):
-        threading.Thread(target=self.worker).start()
-
-    def _worker(self):
-        raise NotImplementedError("Abstract method")
-
-    def worker(self):
-        try:
-            self._worker()
-        except tornado.web.HTTPError, e:
-            self.set_status(e.status_code)
-        except:
-            logging.error("_worker problem", exc_info=True)
-            self.set_status(500)
-        tornado.ioloop.IOLoop.instance().add_callback(self.async_callback(self.results))
-
-    def results(self):
-        if self.get_status() != 200:
-            self.send_error(self.get_status())
-            return
-        if hasattr(self, 'res') and self.res is not None:
-            self.finish(self.res)
-            return
-        if hasattr(self, 'redir'):
-            self.redirect(self.redir)
-            return
-        self.send_error(500)
+def async_task(func):
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        th = threading.Thread(target=func, args=args, kwargs=kwargs)
+        th.start()
+    return async_func
 
 
-class Handler(ThreadMixin):
+class Handler(web.RequestHandler):
     entity = None
     action = None
     data = None
-    res = None
 
-    def _worker(self):
-        self.res = api_executor.execute(self.entity, self.action, self.data)
+    @async_task
+    def retriever(self, callback):
+        res = api_executor.execute(self.entity, self.action, self.data)
+        callback(res)
 
     @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def get(self, entity, action):
         self.entity = entity
         self.action = action
         self.data = parse_get(self.request.arguments)
-        self.start_worker()
+        response = yield tornado.gen.Task(self.retriever)
+        self.finish(response)
 
     @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def post(self, entity, action):
         self.entity = entity
         self.action = action
         self.data = json.loads(self.request.body, encoding='utf-8')
-        self.start_worker()
+        response = yield tornado.gen.Task(self.retriever)
+        self.finish(response)
 
 define('port', type=int, default=8888)
 
