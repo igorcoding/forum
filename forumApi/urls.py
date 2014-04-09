68726fadfda848a4bc1595dedfa6b4d3c7e91685
igorcoding <igorcoding@gmail.com>
2014-04-09 22:03:29 +0400

    Got rid off django

--- forumApi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from forumApi import views
-
-urlpatterns = patterns('',
-    # url(r'^$', views.new_questions, name='index')
-    url(r'^(?P<entity>\S+)/(?P<action>\S+)$', views.index),
-)
